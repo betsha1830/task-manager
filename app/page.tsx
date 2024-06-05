efd50ee07fac35f419f11efc1702dc1443b8ae78
Betsue Weldemariam <btsegay0@gmail.com>
2024-06-05 22:46:03 +0300

    added functionality to filter tasks by status(active, completed)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { tree } from "next/dist/build/templates/app-page"
 import { useState } from "react"
 import { ChangeEvent } from "react"
 
@@ -7,14 +8,16 @@ export default function Home() {
   const [id, setId] = useState(1)
   const initialTask = [{ id: id, task: "", completed: false }]
 
-  const [event, setEvent] = useState(true)
+  const [output, setOutput] = useState(initialTask)
   const [storage, setStorage] = useState(initialTask)
   const [currentInput, setCurrentInput] = useState("")
 
   //  Adds a task to the list
   function addTask() {
     setId(id + 1)
-    setStorage([...storage, { id: id, task: currentInput, completed: false }])
+    const i = [...storage, { id: id, task: currentInput, completed: false }]
+    setStorage(i)
+    setOutput(i)
   }
 
   //  Listens to text input change
@@ -34,18 +37,44 @@ export default function Home() {
       }
     })
     setStorage(temp)
+    setOutput(temp)
   }
 
   //	Deletes a task
   function deleteTask(id: number) {
     let temp = storage.slice(0, id)
-    // setStorage(storage.filter((i) => id !== i.id))
     storage.map((i) => {
       if (i.id <= id) return
       setId(i.id) //	Assigns a new id value for an order list
       temp.push({ id: i.id - 1, task: i.task, completed: i.completed })
     })
     setStorage(temp)
+    setOutput(temp)
+  }
+
+  //	Filters tasks based on its status
+  function filterTasks(filter: string) {
+    let temp = [{ id: 0, task: "", completed: false }]
+    if (filter === "completed") {
+      //	Filters tasks that are completed
+      storage.map((i) => {
+        if (i.completed === true) {
+          temp.push({ id: i.id, task: i.task, completed: i.completed })
+        }
+      })
+      setOutput(temp)
+    } else if (filter === "active") {
+      //	Filters tasks that are still active
+      storage.map((i) => {
+        if (i.completed === false) {
+          temp.push({ id: i.id, task: i.task, completed: i.completed })
+        }
+      })
+      setOutput(temp)
+    } else if (filter === "all") {
+      //	Lists all tasks
+      setOutput(storage)
+    }
   }
 
   return (
@@ -58,9 +87,22 @@ export default function Home() {
       <button className="border-2 border-black p-2" onClick={addTask}>
         Add task
       </button>
+      Filter:{" "}
+      <select className=" w-48">
+        <option value="">Choose an option</option>
+        <option onClick={() => filterTasks("all")} value={"all"}>
+          All
+        </option>
+        <option onClick={() => filterTasks("completed")} value="completed">
+          Completed
+        </option>
+        <option onClick={() => filterTasks("active")} value="active">
+          Active
+        </option>
+      </select>
       <div>
-        {storage.map((c, i) => {
-          if (i === 0) return // Remove the inital task that was used for data type mapping
+        {output.map((c, i) => {
+          if (c.id === 0) return // Remove the inital task that was used for data type mapping
           return (
             <div key={c.id}>
               {c.id}. {c.task}{" "}
